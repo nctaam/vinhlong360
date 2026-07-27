de7efa3fbc26cb04430bc3e6f98afe50fef48724
vinhlong360 <phamhoctriet8882@gmail.com>
2026-07-27 22:27:57 +0700

    test: stabilize frontend environment ownership

--- web-nuxt/vitest.config.ts
+++ web-nuxt/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
+    hookTimeout: 30_000,
     environmentOptions: {
       nuxt: {
         domEnvironment: 'happy-dom',
