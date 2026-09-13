0e247854dbff50fb7c65654cba0f1cf8e1215ab6
vinhlong360 <phamhoctriet8882@gmail.com>
2026-09-13 14:26:49 +0700

    fix(seo): pass aboutJsonLd directly to safeJsonLd in gioi-thieu.vue

--- web-nuxt/pages/gioi-thieu.vue
+++ web-nuxt/pages/gioi-thieu.vue
@@ -172,7 +172,7 @@ const aboutJsonLd = computed(() => buildAboutPageSchemaGraph({
 useHead(() => ({
   link: [{ rel: 'canonical', href: canonicalUrl('/gioi-thieu') }],
   script: [
-    { type: 'application/ld+json', innerHTML: safeJsonLd(aboutJsonLd.value) },
+    { type: 'application/ld+json', innerHTML: safeJsonLd(aboutJsonLd) },
   ],
 }))
 </script>
